1652e1a930a896ccd8e481d14030dd4c7723b1f8
YAGNESH <yagneshsuthar251@gmail.com>
2025-08-22 22:44:09 +0530

    Update Login.jsx

--- client/src/pages/Login.jsx
+++ client/src/pages/Login.jsx
@@ -53,7 +53,7 @@ export default function Login() {
   };
 
   const handleGoogleLogin = () => {
-    window.location.href = 'http://localhost:5000/auth/google';
+    window.location.href = 'https://edu-path-server.vercel.app/auth/google';
   };
 
   return (
